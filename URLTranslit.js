74c884ed69ba0b976a18326845a7e2b3566ca5d3
juek <jk@grafikrausz.at>
2020-06-12 20:26:57 +0200

    omit closing ?> PHP tag

--- URLTranslit.js
+++ URLTranslit.js
@@ -711,7 +711,7 @@ $(function(){
 
 	//do translit and apply changes
 	TranslitSlug = function(e){
-		let separator = '-';
+		let separator = $('#gp_space_char').val() || '-';
 		var $title_inp = $(e.target).closest('#gp_rename_table').find('.title_label.gpinput');
 		var $new_title_inp = $(e.target).closest('#gp_rename_table').find('.new_title.gpinput');
 		var slug = myTransliterate($new_title_inp.val())
