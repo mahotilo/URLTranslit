46cc8d8afa1204cb5777fc5ab484f26ce24a2af9
juek <jk@grafikrausz.at>
2020-06-12 20:27:52 +0200

    Revert "omit closing ?> PHP tag"
    This reverts commit 74c884ed69ba0b976a18326845a7e2b3566ca5d3.

--- URLTranslit.js
+++ URLTranslit.js
@@ -711,7 +711,7 @@ $(function(){
 
 	//do translit and apply changes
 	TranslitSlug = function(e){
-		let separator = $('#gp_space_char').val() || '-';
+		let separator = '-';
 		var $title_inp = $(e.target).closest('#gp_rename_table').find('.title_label.gpinput');
 		var $new_title_inp = $(e.target).closest('#gp_rename_table').find('.new_title.gpinput');
 		var slug = myTransliterate($new_title_inp.val())
